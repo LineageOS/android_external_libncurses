3db41f3dc7b41d4496478994376f061c46a3bf34
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-07-20 01:03:27 +0200

    ncursesw: override bionic MB_LEN_MAX and MB_CUR_MAX
    bionic defines those values to 1, which is not compatible
    with our implementation of wchar_t
    
    Change-Id: I44b0a10830946aa2201641759dae5cffb43f646d

--- include/libutf8.h
+++ include/libutf8.h
@@ -1,6 +1,24 @@
 #ifndef _LIBUTF8_H_
 #define _LIBUTF8_H_ 1
 
+#ifdef __BIONIC__
+
+#ifdef MB_LEN_MAX
+#if MB_LEN_MAX == 1
+#undef MB_LEN_MAX
+#define MB_LEN_MAX 8
+#endif
+#endif
+
+#ifdef MB_CUR_MAX
+#if MB_CUR_MAX == 1
+#undef MB_CUR_MAX
+#define MB_CUR_MAX 4
+#endif
+#endif
+
+#endif /* __BIONIC__ */
+
 /* from widechar/mblen.c */
 int mblen(const char *s, size_t n);
 
