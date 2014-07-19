ec4a90d6db016af82a3559c69d02c994019b5223
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-07-19 23:47:22 +0200

    Restore test suite, with Android.mk
    some test apps are included for libncurses and libncursesw
    
    Change-Id: I31d3436d2b6c1c6ca7d9ebcb69433998d5f6ef4e

---
+++ test/Android.mk
@@ -0,0 +1,47 @@
+LOCAL_PATH:= $(call my-dir)
+
+progs_include := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../ncurses
+widec_flags := -DUSE_WIDECHAR -DUSE_WIDEC_SUPPORT=1 -D_XOPEN_SOURCE_EXTENDED
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := test_inwstr
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncursesw
+LOCAL_CFLAGS += $(widec_flags)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := test_instr
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := xmas
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := demo_defkey
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := view
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncursesw
+LOCAL_CFLAGS += $(widec_flags)
+include $(BUILD_EXECUTABLE)
+
