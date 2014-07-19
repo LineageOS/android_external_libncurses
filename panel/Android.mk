d4c163651b0bef292a0c84b0575e13209ee94ccd
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-07-19 22:36:51 +0200

    progs: remove old binaries, add android Makefile
    Change-Id: I9e8a6bfec9bd51560bbbb27fddc42c663848c8cf

---
+++ panel/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH:= $(call my-dir)
+
+progs_include := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../ncurses
+
+include $(CLEAR_VARS)
+
+srcdir =
+pfiles := \
+    $(srcdir)panel.c \
+    $(srcdir)p_above.c \
+    $(srcdir)p_below.c \
+    $(srcdir)p_bottom.c \
+    $(srcdir)p_delete.c \
+    $(srcdir)p_hide.c \
+    $(srcdir)p_hidden.c \
+    $(srcdir)p_move.c \
+    $(srcdir)p_new.c \
+    $(srcdir)p_replace.c \
+    $(srcdir)p_show.c \
+    $(srcdir)p_top.c \
+    $(srcdir)p_update.c \
+    $(srcdir)p_user.c \
+    $(srcdir)p_win.c
+
+LOCAL_MODULE      := libpanel
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(pfiles)
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_CFLAGS += -DHAVE_CONFIG_H -U_XOPEN_SOURCE -D_XOPEN_SOURCE=500 -U_POSIX_C_SOURCE -D_POSIX_C_SOURCE=199506L
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_STATIC_LIBRARY)
+
---
+++ progs/Android.mk
@@ -0,0 +1,69 @@
+LOCAL_PATH:= $(call my-dir)
+
+progs_include := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../ncurses
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := tic
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c dump_entry.c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+LOCAL_LDFLAGS += -Wl,--no-fatal-warnings
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := toe
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c dump_entry.c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := clear
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := tput
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := tset
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE      := infocmp
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c dump_entry.c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := capconvert
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
+
