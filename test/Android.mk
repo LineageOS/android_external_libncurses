b50c5db1d569c247a5f3c9b0afd50b042c3e4ba6
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-07-20 21:58:34 +0200

    update widechar part and tests from ncurses 5.9
    Change-Id: I63e1ba135c65e4818b4891763cc6999510a3a0dd

--- test/Android.mk
+++ test/Android.mk
@@ -4,7 +4,6 @@ progs_include := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../ncurses
 widec_flags := -DUSE_WIDECHAR -DUSE_WIDEC_SUPPORT=1 -D_XOPEN_SOURCE_EXTENDED
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE      := test_inwstr
 LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
@@ -14,7 +13,6 @@ LOCAL_CFLAGS += $(widec_flags)
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE      := test_instr
 LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
@@ -23,7 +21,14 @@ LOCAL_SHARED_LIBRARIES := libncurses
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE      := xmas
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncurses
+include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE      := demo_defkey
 LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
@@ -32,8 +37,16 @@ LOCAL_SHARED_LIBRARIES := libncurses
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE      := test_addwstr
+LOCAL_MODULE_TAGS := debug
+LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
+LOCAL_C_INCLUDES  := $(progs_include)
+LOCAL_SHARED_LIBRARIES := libncursesw
+LOCAL_CFLAGS += $(widec_flags)
+include $(BUILD_EXECUTABLE)
 
-LOCAL_MODULE      := background
+include $(CLEAR_VARS)
+LOCAL_MODULE      := view
 LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES   := $(LOCAL_MODULE).c
 LOCAL_C_INCLUDES  := $(progs_include)
